fdcd509c84df91d1269e7748bd791faba6bcc2a5
jrildosilva <jgjsguilherme@gmail.com>
2021-06-24 00:37:43 -0300

    funcoes.js

---
+++ funcoes.js
@@ -0,0 +1,22 @@
+// funções.js 
+//let x = "";
+
+//console.log = (x);
+
+//x = "oi";
+// 1) declara a função
+                       // string
+
+//function imprimetexto(texto){
+    //console.log (texto)
+//}
+// 2) executa a funçao ( 1 ou + vezes)
+
+//imprimetexto("oi");
+//imprimetexto("crânio");
+//impremetexto("disciplina");
+function soma() {
+    return 2+ 2;
+}
+console.log(soma());
+
